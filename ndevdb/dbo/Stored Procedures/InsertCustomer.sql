533f54240f7aac99f7db840bde7a1e6b5996c2f4
Nikolai Thomassen <niktho@gmail.com>
2019-02-21 23:08:58 +0100

    Add table and procedure to demo

---
+++ ndevdb/dbo/Stored Procedures/InsertCustomer.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE PROCEDURE dbo.InsertCustomer
+(
+	@FirstName	NVARCHAR(255),
+	@MiddleName NVARCHAR(255),
+	@LastName	NVARCHAR(255),
+	@Age		INT			 
+)
+AS 
+BEGIN
+SET NOCOUNT ON;
+
+
+SET NOCOUNT OFF;
+END
---
+++ ndevdb/dbo/Tables/Customer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]  NVARCHAR (255) NOT NULL,
+    [MiddleName] NVARCHAR (255) NULL,
+    [LastName]   NVARCHAR (255) NOT NULL,
+    [Age]        INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
